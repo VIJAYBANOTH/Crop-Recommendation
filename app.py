4dc341e67a3f1968696a585922fd39eafbc8e2e8
Abhishek-AI-DEV-7 <abhishekgaddam09@gmail.com>
2024-02-19 22:10:18 +0530

    Added folder structure

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask,request,render_template
 import numpy as np
-import pandas
-import sklearn
 import pickle
 
 # importing model
-model = pickle.load(open('model.pkl','rb'))
-sc = pickle.load(open('standscaler.pkl','rb'))
-ms = pickle.load(open('minmaxscaler.pkl','rb'))
+model = pickle.load(open('models/model.pkl','rb'))
+sc = pickle.load(open('models/standscaler.pkl','rb'))
+ms = pickle.load(open('models/minmaxscaler.pkl','rb'))
 
 # creating flask app
 app = Flask(__name__)
